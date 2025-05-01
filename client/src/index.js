4ff8f1128f013cc8a0239be10a900b8293e6f73d
Maksym <selyavinmax100299@gmail.com>
2025-05-01 23:35:23 +0200

    ♻️ Add cache clearing on load to prevent stale JS issues

--- client/src/index.js
+++ client/src/index.js
@@ -5,3 +5,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
+// Очистка кеша браузера при загрузке (если поддерживается)
+if ('caches' in window) {
+    caches.keys().then(names => {
+      for (let name of names) caches.delete(name);
+    });
+  }
+  
