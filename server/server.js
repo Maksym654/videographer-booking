4649a14645e51815491d331f00d84bad4a04ea40
Maksym <selyavinmax100299@gmail.com>
2025-05-02 17:46:00 +0200

    Запущены изменения для серверной папки

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ const { generateCalendarLink, formatDateTime } = require('./utils/calendarUtils'
 
 // Firebase Admin SDK с конфигом из переменной окружения
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG.replace(/\\n/g, '\n'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
