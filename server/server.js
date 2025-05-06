14d1bd927833e78d8d0d60e4f642ee2408357496
Maksym <selyavinmax100299@gmail.com>
2025-05-06 12:40:46 +0200

    💳 Stripe: включён автоматический выбор метода оплаты

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.post('/create-checkout-session', async (req, res) => {
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+     // payment_method_types: ['card'],
       mode: 'payment',
       customer_email: email,
       line_items: [{
